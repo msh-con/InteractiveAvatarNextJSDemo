fadf9d5981460f97016b95b6cbae160965347520
msh-con <info@msh.info>
2026-01-29 17:16:55 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  swcMinify: true,
-}
-
-module.exports = nextConfig
